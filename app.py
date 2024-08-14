bd538b2f615d91afda21751e7d5e791e7d6338ad
vignesh <dineshad139@gmail.com>
2024-08-14 14:36:27 +0530

    Push check

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def test():
     return "success", 200
 
 
+@app.route("/", methods=["POST"])
+def test():
+    return "post success", 200
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
