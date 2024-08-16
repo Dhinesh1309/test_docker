d7483dae6b17da09a1fef3a8093aa226aab1890c
vignesh <dineshad139@gmail.com>
2024-08-16 12:26:03 +0530

    put method is removed

--- app.py
+++ app.py
@@ -3,19 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def test():
+def test_get():
     return "success", 200
 
 
 @app.route("/", methods=["POST"])
-def test():
+def test_post():
     return "post success", 200
-
-
-@app.route("/", methods=["PUT"])
-def test_put():
-    return "post success", 200
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
