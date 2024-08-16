2c144ee580c7416235200421206d89277336ba48
vignesh <dineshad139@gmail.com>
2024-08-16 12:24:45 +0530

    put is added

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def test():
     return "post success", 200
 
 
+@app.route("/", methods=["PUT"])
+def test_put():
+    return "post success", 200
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
